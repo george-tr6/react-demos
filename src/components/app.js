a14ccf400ac3a6a23a694c316ef3ed51af8094ec
george-tr6 <george.tr6@gmail.com>
2018-09-13 10:13:54 -0700

    demo completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import Greeting from './greeting';
 
 const App = () => (
     <div>
         <div className="app">
+            <Greeting name="Jim Bob" favColor="blue" />
+            <Greeting name="Samantha John" favColor="hotpink" />
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
         </div>
