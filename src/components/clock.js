4e98d187f3d3e12fa82040e03e3b38a5c1cf2074
george-tr6 <george.tr6@gmail.com>
2018-09-15 15:32:37 -0700

    clock challenge completed

---
+++ src/components/clock.js
@@ -0,0 +1,32 @@
+
+import React, { Component } from 'react';
+import Time from './time';
+
+
+class Clock extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            time: new Date()
+        }
+    }
+    
+    componentDidMount(){
+        setInterval(()=> this.tick(), 1000);
+    }
+
+    tick(){
+        this.setState({
+            time: new Date()
+        });
+    }
+    render(){
+        return (
+            <div>
+                <h1> The time is: <Time time={this.state.time} /></h1>
+            </div>
+        );
+    }
+}
+export default Clock;
