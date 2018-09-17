3bda3cc90bfa6856ab06080fd7fed4a3868654c9
george-tr6 <george.tr6@gmail.com>
2018-09-17 14:33:56 -0700

    demo form completed

---
+++ src/components/demo_form.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+ class DemoForm extends Component {
+     constructor(props){
+        super(props);
+
+        console.log('Props:', props);
+
+        this.state = {
+            email: props.email || '',
+            password: '',
+            name:''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+     }
+
+     handleSubmit(event){
+         event.preventDefault();
+        console.log('Form Submitted:', this.state);
+
+        this.setState({
+            email: '',
+            password: '',
+            name:'' 
+        });
+     }
+
+     handleChange(event){
+         this.setState({
+             [event.target.name]: event.target.value //brakets allow us to set dynamic values
+         });
+         
+        //  switch(event.target.name){
+        //      case 'email':
+        //         this.setState({email: event.target.value});
+        //         break;
+        //     case 'password':
+        //         this.setState({password: event.target.value});
+        //         break;
+        //     case 'name':
+        //         this.setState({name: event.target.value});
+        //         break;
+
+        //  }
+     }
+
+     render(){
+         return (
+             <form onSubmit={this.handleSubmit.bind(this)}>
+                 <h1>Demo Form</h1>
+                 <div>
+                     <label>Email</label>
+                     <input 
+                        type="text"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        autoComplete='off'
+                    />
+                 </div>
+                 <div>
+                     <label>Name</label>
+                     <input
+                        type='text'
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                     />
+                 </div>
+                 <div>
+                     <label>Password</label>
+                     <input
+                         type="password"
+                         name="password" 
+                         value={this.state.password}
+                         onChange={this.handleChange}
+                    />
+                 </div>
+                 <button>Submit Form</button>
+             </form>
+         );
+     }
+ }
+
+ export default DemoForm;
